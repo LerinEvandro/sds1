0b42f4aa07463f764de608b5a4cd437ff7a3fbac
Evandro Lerin <evandrolerin@msn.com>
2020-09-21 04:21:11 -0300

    Configurando Netlify

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -22,7 +22,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-lerin.herokuapp.com';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
@@ -83,7 +83,7 @@ const Charts = () => {
                             width="350"
                         />
                     </div>
-                </div>
+                </div>            
             </div>
         </div>
     )
